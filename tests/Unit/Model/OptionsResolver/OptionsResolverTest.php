d9edf236a7a21a08e55bbde55ad8b3a7b5f4382b
Diogo Teixeira <diogo.teixeira@wondeotec.com>
2016-03-02 13:48:22 +0000

    Added support for accepting undefined options. Improved test coverage.

--- tests/Unit/Model/OptionsResolver/OptionsResolverTest.php
+++ tests/Unit/Model/OptionsResolver/OptionsResolverTest.php
@@ -14,6 +14,12 @@ namespace EBT\OptionsResolver\Tests\Unit\Model\OptionsResolver;
 use EBT\OptionsResolver\Model\OptionsResolver\OptionsResolver;
 use EBT\OptionsResolver\Tests\BaseUnitTestCase;
 
+/**
+ * EBT\OptionsResolver\Tests\Unit\Model\OptionsResolver\OptionsResolverTest
+ *
+ * @coversDefaultClass EBT\OptionsResolver\Model\OptionsResolver\OptionsResolver
+ * @group              unit
+ */
 class OptionsResolverTest extends BaseUnitTestCase
 {
     /**
@@ -26,6 +32,8 @@ class OptionsResolverTest extends BaseUnitTestCase
      *
      * @expectedException        \Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException
      * @expectedExceptionMessage The option "test_option" does not exist
+     *
+     * @covers ::setCast
      */
     public function testSetCastUndefinedOptionFailure()
     {
@@ -37,6 +45,8 @@ class OptionsResolverTest extends BaseUnitTestCase
      *
      * @expectedException        \Symfony\Component\OptionsResolver\Exception\InvalidArgumentException
      * @expectedExceptionMessage The type cast "test_type" does not exist
+     *
+     * @covers ::setCast
      */
     public function testSetCastUnsupportedCastFailure()
     {
@@ -49,6 +59,8 @@ class OptionsResolverTest extends BaseUnitTestCase
      *
      * @expectedException        \Symfony\Component\OptionsResolver\Exception\InvalidArgumentException
      * @expectedExceptionMessage The given closure must define exactly one required parameter (the value to cast).
+     *
+     * @covers ::setCast
      */
     public function testSetCastClosureWithWrongNumberOfArgumentsFailure()
     {
@@ -67,6 +79,8 @@ class OptionsResolverTest extends BaseUnitTestCase
      *
      * @expectedException        \Symfony\Component\OptionsResolver\Exception\InvalidArgumentException
      * @expectedExceptionMessage The given closure must define exactly one required parameter (the value to cast).
+     *
+     * @covers ::setCast
      */
     public function testSetCastClosureWithWrongNumberOfRequiredArgumentsFailure()
     {
@@ -82,6 +96,8 @@ class OptionsResolverTest extends BaseUnitTestCase
 
     /**
      * Tests that the isCast method returns true when a cast is defined and false when it is not.
+     *
+     * @covers ::isCast
      */
     public function testIsCast()
     {
@@ -105,6 +121,8 @@ class OptionsResolverTest extends BaseUnitTestCase
      * @dataProvider invalidSetCastDataProvider
      *
      * @expectedException \EBT\OptionsResolver\Exception\ResolverException
+     *
+     * @covers ::setCast
      */
     public function testSetCastFailure($option, $allowedTypes, $cast, $value)
     {
@@ -112,7 +130,7 @@ class OptionsResolverTest extends BaseUnitTestCase
         $this->optionsResolver->setAllowedTypes($option, $allowedTypes);
         $this->optionsResolver->setCast($option, $cast);
 
-        $this->optionsResolver->resolve(array($option => $value));
+        $this->optionsResolver->resolve([$option => $value]);
     }
 
     /**
@@ -125,18 +143,132 @@ class OptionsResolverTest extends BaseUnitTestCase
      * @param string $expectedType Expected option type after resolved.
      *
      * @dataProvider validSetCastDataProvider
+     *
+     * @covers ::setCast
+     * @covers ::resolveCasts
+     * @covers ::castToType
      */
     public function testSetCastSuccess($option, $allowedTypes, $cast, $value, $expectedType)
     {
         $this->optionsResolver->setDefined($option);
         $this->optionsResolver->setAllowedTypes($option, $allowedTypes);
         $this->optionsResolver->setCast($option, $cast);
+        $this->optionsResolver->setDefined('option_not_to_cast');
+        $options = [
+            $option              => $value,
+            'option_not_to_cast' => '1',
+        ];
 
-        $result = $this->optionsResolver->resolve(array($option => $value));
-
+        $result = $this->optionsResolver->resolve($options);
         $this->assertInternalType('array', $result);
         $this->assertArrayHasKey($option, $result);
+        $this->assertArrayHasKey('option_not_to_cast', $result);
         $this->assertEquals($expectedType, gettype($result[$option]));
+        $this->assertEquals('string', gettype($result['option_not_to_cast']));
+    }
+
+    /**
+     * Tests that closure based casts work as expected.
+     *
+     * @covers ::setCast
+     * @covers ::resolveCasts
+     */
+    public function testSetCastClosureSuccess()
+    {
+        $this->optionsResolver->setDefined('option1');
+        $this->optionsResolver->setCast(
+            'option1',
+            function ($value) {
+
+                return (int) $value;
+            }
+        );
+
+        $result = $this->optionsResolver->resolve(['option1' => '123']);
+        $this->assertEquals(['option1' => 123], $result);
+    }
+
+    /**
+     * Tests that undefined options can be ignore if the appropriate flag is set, and are removed from the result.
+     *
+     * @covers ::resolve
+     * @covers ::clearUndefinedOptions
+     */
+    public function testResolveUndefinedOptionsAllowed()
+    {
+        $options = [
+            'option1' => 1,
+            'option2' => 2,
+        ];
+        $this->optionsResolver->setRequired('option1');
+
+        $result = $this->optionsResolver->resolve($options, true);
+        $this->assertEquals(['option1' => 1], $result);
+    }
+
+    /**
+     * Tests that undefined options still raise exceptions by default.
+     *
+     * @expectedException \EBT\OptionsResolver\Exception\ResolverException
+     * @expectedExceptionMessage option2
+     *
+     * @covers ::resolve
+     * @covers ::clearUndefinedOptions
+     */
+    public function testResolveUndefinedOptionsNotAllowed()
+    {
+        $options = [
+            'option1' => 1,
+            'option2' => 2,
+        ];
+        $this->optionsResolver->setRequired('option1');
+
+        $this->optionsResolver->resolve($options);
+    }
+
+    /**
+     * Tests that when cleared is called the defined casts are also removed.
+     *
+     * @covers ::clear
+     */
+    public function testClear()
+    {
+        $this->optionsResolver->setDefined('option1');
+        $this->optionsResolver->setRequired('option1');
+        $this->optionsResolver->setCast('option1', 'int');
+
+        $this->assertTrue($this->optionsResolver->isDefined('option1'));
+        $this->assertTrue($this->optionsResolver->isRequired('option1'));
+        $this->assertTrue($this->optionsResolver->isCast('option1'));
+
+        $this->optionsResolver->clear();
+        $this->assertFalse($this->optionsResolver->isDefined('option1'));
+        $this->assertFalse($this->optionsResolver->isRequired('option1'));
+        $this->assertFalse($this->optionsResolver->isCast('option1'));
+    }
+
+    /**
+     * Tests that when options are removed their defined type casts are also removed.
+     *
+     * @covers ::remove
+     */
+    public function testRemove()
+    {
+        $this->optionsResolver->setDefined('option1');
+        $this->optionsResolver->setCast('option1', 'int');
+        $this->optionsResolver->setDefined('option2');
+        $this->optionsResolver->setCast('option2', 'int');
+
+        $this->assertTrue($this->optionsResolver->isDefined('option1'));
+        $this->assertTrue($this->optionsResolver->isCast('option1'));
+        $this->assertTrue($this->optionsResolver->isDefined('option2'));
+        $this->assertTrue($this->optionsResolver->isCast('option2'));
+
+        $this->optionsResolver->remove('option1');
+        $this->assertFalse($this->optionsResolver->isDefined('option1'));
+        $this->assertFalse($this->optionsResolver->isCast('option1'));
+        $this->assertTrue($this->optionsResolver->isDefined('option2'));
+        $this->assertTrue($this->optionsResolver->isCast('option2'));
     }
 
     /**
@@ -146,15 +278,15 @@ class OptionsResolverTest extends BaseUnitTestCase
      */
     public function invalidSetCastDataProvider()
     {
-        return array(
-            array('test', 'int', 'int', '123a'),
-            array('test', 'bool', 'bool', 'y'),
-            array('test', 'bool', 'bool', '0.0'),
-            array('test', 'bool', 'bool', '1.0'),
-            array('test', 'int', 'int', '123.0'),
-            array('test', 'int', 'int', 123.5),
-            array('test', 'int', 'float', 123.5),
-            array(
+        return [
+            ['test', 'int', 'int', '123a'],
+            ['test', 'bool', 'bool', 'y'],
+            ['test', 'bool', 'bool', '0.0'],
+            ['test', 'bool', 'bool', '1.0'],
+            ['test', 'int', 'int', '123.0'],
+            ['test', 'int', 'int', 123.5],
+            ['test', 'int', 'float', 123.5],
+            [
                 'test',
                 'int',
                 function ($value) {
@@ -162,8 +294,8 @@ class OptionsResolverTest extends BaseUnitTestCase
                     return is_int($value) ? (int) $value : $value;
                 },
                 'not an integer',
-            ),
-        );
+            ],
+        ];
     }
 
     /**
@@ -173,30 +305,30 @@ class OptionsResolverTest extends BaseUnitTestCase
      */
     public function validSetCastDataProvider()
     {
-        return array(
-            array('test', 'bool', 'bool', 'true', 'boolean'),
-            array('test', 'bool', 'bool', 'yes', 'boolean'),
-            array('test', 'bool', 'bool', 1, 'boolean'),
-            array('test', 'bool', 'bool', 1.0, 'boolean'),
-            array('test', 'bool', 'bool', '1', 'boolean'),
-            array('test', 'bool', 'bool', 'false', 'boolean'),
-            array('test', 'bool', 'bool', 'no', 'boolean'),
-            array('test', 'bool', 'bool', 0, 'boolean'),
-            array('test', 'bool', 'bool', 0.0, 'boolean'),
-            array('test', 'bool', 'bool', '0', 'boolean'),
-            array('test', 'string', 'int', '123a', 'string'),
-            array('test', 'int', 'int', 123.0, 'integer'),
-            array('test', 'int', 'int', '10', 'integer'),
-            array('test', 'int', 'int', '     10', 'integer'),
-            array('test', 'int', 'int', '10     ', 'integer'),
-            array('test', 'int', 'int', '          10         ', 'integer'),
-            array('test', 'float', 'float', 123, 'double'),
-            array('test', 'float', 'float', '10', 'double'),
-            array('test', 'float', 'float', '10.5', 'double'),
-            array('test', 'float', 'float', '     10.5', 'double'),
-            array('test', 'float', 'float', '10.5     ', 'double'),
-            array('test', 'float', 'float', '          10         ', 'double'),
-            array(
+        return [
+            ['test', 'bool', 'bool', 'true', 'boolean'],
+            ['test', 'bool', 'bool', 'yes', 'boolean'],
+            ['test', 'bool', 'bool', 1, 'boolean'],
+            ['test', 'bool', 'bool', 1.0, 'boolean'],
+            ['test', 'bool', 'bool', '1', 'boolean'],
+            ['test', 'bool', 'bool', 'false', 'boolean'],
+            ['test', 'bool', 'bool', 'no', 'boolean'],
+            ['test', 'bool', 'bool', 0, 'boolean'],
+            ['test', 'bool', 'bool', 0.0, 'boolean'],
+            ['test', 'bool', 'bool', '0', 'boolean'],
+            ['test', 'string', 'int', '123a', 'string'],
+            ['test', 'int', 'int', 123.0, 'integer'],
+            ['test', 'int', 'int', '10', 'integer'],
+            ['test', 'int', 'int', '     10', 'integer'],
+            ['test', 'int', 'int', '10     ', 'integer'],
+            ['test', 'int', 'int', '          10         ', 'integer'],
+            ['test', 'float', 'float', 123, 'double'],
+            ['test', 'float', 'float', '10', 'double'],
+            ['test', 'float', 'float', '10.5', 'double'],
+            ['test', 'float', 'float', '     10.5', 'double'],
+            ['test', 'float', 'float', '10.5     ', 'double'],
+            ['test', 'float', 'float', '          10         ', 'double'],
+            [
                 'test',
                 'bool',
                 function ($value) {
@@ -205,8 +337,8 @@ class OptionsResolverTest extends BaseUnitTestCase
                 },
                 'si',
                 'boolean',
-            ),
-        );
+            ],
+        ];
     }
 
     /**
